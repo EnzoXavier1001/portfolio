e503515277aec7c9672edc887e13127cc9dbec46
Enzo Costa <enzo_costa7@hotmail.com>
2024-12-30 12:01:51 -0300

    refactor: create utils

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,57 +1,9 @@
+import { allTechnologies } from '../../utils/icons';
 import * as C from './styles'
 
-import javascriptIcon from '../../assets/javascript.svg'
-import typescriptIcon from '../../assets/typescript.svg'
-import reactIcon from '../../assets/react.svg'
-import nodeIcon from '../../assets/nodejs.svg'
-import gitIcon from '../../assets/git.svg'
-import htmlIcon from '../../assets/html.svg'
-import postgreIcon from '../../assets/postgresql.svg'
-import cssIcon from '../../assets/css.svg'
-import figmaIcon from '../../assets/figma.svg'
-
 export const Skills = () => {
-    const allTechnologies = [
-        {
-            icon: javascriptIcon,
-            name: 'JavaScript'
-        },
-        {
-            icon: typescriptIcon,
-            name: 'TypeScript'
-        },
-        {
-            icon: reactIcon,
-            name: 'React'
-        },
-        {
-            icon: nodeIcon,
-            name: 'Node'
-        },
-        {
-            icon: postgreIcon,
-            name: 'PostgreSQL'
-        },
-        {
-            icon: gitIcon,
-            name: 'Git'
-        },
-        {
-            icon: htmlIcon,
-            name: 'HTML'
-        },
-        {
-            icon: cssIcon,
-            name: 'CSS'
-        },
-        {
-            icon: figmaIcon,
-            name: 'Figma'
-        }
-    ];
-
     return (
-        <C.SkillsContainer>
+        <C.SkillsContainer id='skills'>
             <h1>Habilidades</h1>
             <p>Habilidades e tecnologias que eu trabalho</p>
 
---
+++ src/utils/icons.ts
@@ -0,0 +1,48 @@
+import javascriptIcon from '../assets/javascript.svg'
+import typescriptIcon from '../assets/typescript.svg'
+import reactIcon from '../assets/react.svg'
+import nodeIcon from '../assets/nodejs.svg'
+import gitIcon from '../assets/git.svg'
+import htmlIcon from '../assets/html.svg'
+import postgreIcon from '../assets/postgresql.svg'
+import cssIcon from '../assets/css.svg'
+import figmaIcon from '../assets/figma.svg'
+
+export const allTechnologies = [
+    {
+        icon: javascriptIcon,
+        name: 'JavaScript'
+    },
+    {
+        icon: typescriptIcon,
+        name: 'TypeScript'
+    },
+    {
+        icon: reactIcon,
+        name: 'React'
+    },
+    {
+        icon: nodeIcon,
+        name: 'Node'
+    },
+    {
+        icon: postgreIcon,
+        name: 'PostgreSQL'
+    },
+    {
+        icon: gitIcon,
+        name: 'Git'
+    },
+    {
+        icon: htmlIcon,
+        name: 'HTML'
+    },
+    {
+        icon: cssIcon,
+        name: 'CSS'
+    },
+    {
+        icon: figmaIcon,
+        name: 'Figma'
+    }
+];
